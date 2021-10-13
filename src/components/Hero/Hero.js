b9aa2628debc34f2d1e64e7f2eeb8fae8baa356d
boularbahsmail <qiranorane55@gmail.com>
2021-10-13 23:42:28 +0100

    Work environment state : Ready :+1:

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Import css
+import './css/hero.css';
+
+const Hero = () => {
+    return (
+        <div>
+            <h1>State Of Moroccan Developers</h1>
+            <h5>Submit your state as a moroccan developer</h5>
+        </div>
+    )
+}
+
+export default Hero
